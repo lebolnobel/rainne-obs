8dc60468b0f64665b6421621292645d33f68cdd4
Lionel Lebon <zenbom@gmail.com>
2026-03-06 09:17:20 +0100

    feat: clear storage on error page

--- src/app/components/ErrorPage.tsx
+++ src/app/components/ErrorPage.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
-import { useRouteError, isRouteErrorResponse, NavLink } from 'react-router-dom';
+import {
+  useRouteError,
+  isRouteErrorResponse,
+  NavLink,
+  useNavigate,
+} from 'react-router-dom';
 
 const ErrorPage = (): React.ReactNode => {
+  const navigate = useNavigate();
   const error = useRouteError();
   let errorMessage: string;
 
@@ -10,18 +16,20 @@ const ErrorPage = (): React.ReactNode => {
     errorMessage = error.statusText;
   } else if (error instanceof Error) {
     // Clear in case of issue with stored data
-    // localStorage.clear();
     errorMessage = error.message;
   } else if (typeof error === 'string') {
     // Clear in case of issue with stored data
-    // localStorage.clear();
     errorMessage = error;
   } else {
     // Clear in case of issue with stored data
-    // localStorage.clear();
     console.error(error);
     errorMessage = 'Ahem...';
   }
+
+  const reset = () => {
+    localStorage.clear();
+    navigate('/');
+  };
 
   return (
     <section>
@@ -34,13 +42,22 @@ const ErrorPage = (): React.ReactNode => {
           <p className="mb-4 text-lg font-light text-gray-500">
             {errorMessage}
           </p>
-          <NavLink
-            to="/"
-            title="Retour à l'accueil"
-            className="px-6 py-2 font-semibold rounded-md bg-natagora text-white hover:bg-natagora/90 hover:shadow focus:outline-none focus:ring-2 focus:ring-natagora/30"
-          >
-            Retour
-          </NavLink>
+
+          <div className="flex gap-4 justify-center">
+            <NavLink
+              to="/"
+              title="Retour à l'accueil"
+              className="px-6 py-2 font-semibold rounded-md bg-natagora text-white hover:bg-natagora/90 hover:shadow focus:outline-none focus:ring-2 focus:ring-natagora/30"
+            >
+              Retour
+            </NavLink>
+            <button
+              onClick={reset}
+              className="px-6 py-2 font-semibold rounded-md bg-red-700 text-white hover:bg-red-600 hover:shadow focus:outline-none focus:ring-2 focus:ring-red-400/40"
+            >
+              Réinitialiser
+            </button>
+          </div>
         </div>
       </div>
     </section>
